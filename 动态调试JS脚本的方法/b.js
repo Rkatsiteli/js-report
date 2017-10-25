1f7f3a0eea0158df761f28a0f73fa54fb5478e0d
Rkatsiteli <921702356@qq.com>
2017-10-25 20:56:20 +0800

    调试动态js脚本
    通过  sourceURL 与 debugger 进行调试动态的JS 文件

---
+++ 动态调试JS脚本的方法/b.js
@@ -0,0 +1,17 @@
+/**
+ * 方式1: //@ sourceURL=b.js（源映射）
+ * PS：@与sourceURL 之间保留空格
+ * 
+ * 方式2：debugger
+ * 在要调试的地方加上 debugger
+ * 
+ * 
+ */
+
+
+//@ sourceURL=b.js
+
+debugger
+console.log(123);
+debugger 
+console.log(456);
